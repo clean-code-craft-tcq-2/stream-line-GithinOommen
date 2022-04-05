99f4f8bbe19b4b5f748188552b6ece357f11d3dc
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-05 11:37:20 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -20,10 +20,6 @@ float parameter1, parameter2, parameter3;
         charge[i]=parameter3;     
    }
 
-//printf("\nValue of parameter1=%f\tparameter2=%f\tparameter3=%f", temp[i],soc[i],charge[i]);
-        
-   //printf("\nValue of parameter1=%f\tparameter2=%f\tparameter3=%f", parameter1,parameter2,parameter3);
-  // printf("\nValue of parameter1=%f\tparameter2=%f\tparameter3=%f", temp[0],soc[0],charge[0]);
    fclose(fptr); 
   
   // return 0;
