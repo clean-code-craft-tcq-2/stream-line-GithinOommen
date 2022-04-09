fc490e04dd415958030f75ef46a84bef7b50670b
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-09 14:23:28 +0530

    Update Sender.h

--- Send/Sender.h
+++ Send/Sender.h
@@ -2,6 +2,6 @@
 #define SENDER_H
 #include "Header.h"
 
-void ReadFile(float *temp, float *soc, float *charge);
+void ReadFile(char * filename,float *temp, float *soc, float *charge);
 void DispReadData(float *temp, float *soc, float *charge);
 #endif
