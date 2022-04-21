988791632a2c95ba4ec51626167f2d2b6c258bd3
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 13:18:46 +0530

    Update Sender.h

--- Send/Sender.h
+++ Send/Sender.h
@@ -12,4 +12,8 @@ typedef enum
 FileStatus ReadFile(int *SampleCount,char * filename,float *temp, float *soc, float *charge);
 void DispReadData(float *temp, float *soc, float *charge);
 FileStatus FileOpenStatus (char *filepath);
+extern int SampleCount;
+extern float Temperature[50];
+extern  float StateOfCharge[50];
+extern  float ChargeRate[50]
 #endif
