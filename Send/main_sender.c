4af090c9b1823851ab17e3c6cd5cd163072aa168
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-04-21 13:36:34 +0530

    Update main_sender.c

--- Send/main_sender.c
+++ Send/main_sender.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 int main()
 {
-  char *filename = "./Send/Sender.txt";
-  ReadFile( &SampleCount,filename,Temperature,SoC,ChargeRate);
-    DispReadData( Temperature,SoC,ChargeRate);
+ // char *filename = "./Send/Sender.txt";
+ // ReadFile( &SampleCount,filename,Temperature,SoC,ChargeRate);
+  //  DispReadData( Temperature,SoC,ChargeRate);
   
   return 0;
 }
