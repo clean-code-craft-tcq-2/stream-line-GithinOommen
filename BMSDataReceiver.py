c19ee4a19983a9d063982da89d98dc87435ca26d
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-04-21 20:22:00 +0530

    Add BMS Data Receiver and tests Add calculate moving average

--- BMSDataReceiver.py
+++ BMSDataReceiver.py
@@ -1,6 +1,9 @@
 import sys
 from data_config import test_data
 from data_config import dictionary_keys
+from data_config import min_max_print_format
+from data_config import moving_average_format
+from data_config import test_data_null
 
 
 class BMSDataReceiver:
@@ -8,9 +11,10 @@ class BMSDataReceiver:
         self.bms_data = []
         self.max_reading = {}
         self.min_reading = {}
+        self.moving_average = {}
 
-    def stub_fetch_data_from_sender(self):
-        data = test_data
+    def stub_fetch_data_from_sender(self, data):
+        data = test_data_null
         return data
 
     def fetch_data_from_sender(self):
@@ -28,19 +32,46 @@ class BMSDataReceiver:
             self.bms_data.append(data_instance)
         return self.bms_data
 
-    def get_maximum_of_parameters(self):
+    def calculate_maximum_of_parameters(self):
         for key in self.bms_data[0].keys():
             self.max_reading[key] = max(d[key] for d in self.bms_data)
         return self.max_reading
 
-    def get_minimum_of_parameters(self):
+    def calculate_minimum_of_parameters(self):
         for key in self.bms_data[0].keys():
             self.min_reading[key] = min(d[key] for d in self.bms_data)
         return self.min_reading
 
+    def print_min_and_max_values_to_console(self):
+        for key in self.min_reading.keys():
+            self.print_to_console(min_max_print_format.format(key, self.min_reading[key], self.max_reading[key]))
+
+    def print_moving_average(self):
+        for key in self.moving_average.keys():
+            self.print_to_console(moving_average_format.format(key, self.moving_average[key]))
+
+    def calculate_moving_average(self):
+
+        if len(self.bms_data) > 5:
+            for key in self.bms_data[0].keys():
+                moving_average = sum(float(d[key]) for d in self.bms_data[-5:]) / 5
+                self.moving_average[key] = moving_average
+            return self.moving_average
+
+    def print_to_console(self, message):
+        print(message)
+
+    def fetch_and_display_stats_sender_data(self):
+        self.convert_to_list_of_dictionery(self.fetch_data_from_sender())
+        if self.bms_data:
+            self.calculate_maximum_of_parameters()
+            self.calculate_minimum_of_parameters()
+            self.print_min_and_max_values_to_console()
+            self.calculate_moving_average()
+            self.print_moving_average()
+        else:
+            return 'No Data Found'
+
 
 receiver = BMSDataReceiver()
-data_from_sender = receiver.fetch_data_from_sender()
-receiver.convert_to_list_of_dictionery(data_from_sender)
-print(receiver.get_maximum_of_parameters())
-print(receiver.get_minimum_of_parameters())
+receiver.fetch_and_display_stats_sender_data()
--- test_BMSDataReceiver.py
+++ test_BMSDataReceiver.py
@@ -1,14 +1,16 @@
 from BMSDataReceiver import BMSDataReceiver
 from data_config import test_data
 from data_config import dictionary_keys
-
+from data_config import min_max_print_format
+from data_config import moving_average_format
+from data_config import test_data_null
 import unittest
 
 
 class BMSDataReceiverTest(unittest.TestCase):
     def test_stub_fetch_data_from_sender(self):
         receiver = BMSDataReceiver()
-        data = receiver.stub_fetch_data_from_sender()
+        data = receiver.stub_fetch_data_from_sender(test_data)
         self.assertTrue(len(data) > 0)
         self.assertEqual(data, test_data)
 
@@ -29,15 +31,40 @@ class BMSDataReceiverTest(unittest.TestCase):
                           {'Temperature': '3.000000', 'SoC': '2.000000', 'Charge': '0.170000'},
                           {'Temperature': '31.000000', 'SoC': '93.000000', 'Charge': '0.190000'}]
                          )
+
     def test_get_maximum_of_parameters(self):
         receiver = BMSDataReceiver()
         receiver.convert_to_list_of_dictionery(test_data)
-        self.assertEqual(receiver.get_maximum_of_parameters(), {'Temperature': '9.000000', 'SoC': '96.000000', 'Charge': '0.190000'})
+        self.assertEqual(receiver.calculate_maximum_of_parameters(),
+                         {'Temperature': '9.000000', 'SoC': '96.000000', 'Charge': '0.190000'})
 
     def test_get_minimum_of_parameters(self):
         receiver = BMSDataReceiver()
         receiver.convert_to_list_of_dictionery(test_data)
-        self.assertEqual(receiver.get_minimum_of_parameters(), {'Temperature': '10.000000', 'SoC': '2.000000', 'Charge': '0.010000'})
+        self.assertEqual(receiver.calculate_minimum_of_parameters(),
+                         {'Temperature': '10.000000', 'SoC': '2.000000', 'Charge': '0.010000'})
+
+    def test_print_min_and_max_values_to_console(self):
+        receiver = BMSDataReceiver()
+        receiver.convert_to_list_of_dictionery(test_data)
+        min_reading = receiver.calculate_minimum_of_parameters()
+        max_reading = receiver.calculate_maximum_of_parameters()
+        self.assertEqual(
+            min_max_print_format.format('Temperature', min_reading['Temperature'], max_reading['Temperature']),
+            'Minimum and Maximum Temperature are 10.000000 , 9.000000')
+
+    def test_calculate_moving_average_and_format(self):
+        receiver = BMSDataReceiver()
+        receiver.convert_to_list_of_dictionery(test_data)
+        moving_average = receiver.calculate_moving_average()
+        self.assertEqual(moving_average, {'Temperature': 25.6, 'SoC': 67.6, 'Charge': 0.15})
+        self.assertEqual(moving_average_format.format('Temperature', moving_average['Temperature']),
+                         'The moving average of Temperature is 25.6')
+
+    def test_for_null_data(self):
+        receiver = BMSDataReceiver()
+        receiver.convert_to_list_of_dictionery(test_data_null)
+        self.assertEqual(receiver.fetch_and_display_stats_sender_data(), 'No Data Found')
 
 
 unittest.main()
