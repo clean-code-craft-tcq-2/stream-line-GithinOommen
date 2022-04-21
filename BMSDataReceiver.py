b78caa8ed40e2ae559e333aee6729641c931d0af
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-04-21 14:58:02 +0530

    Add BMS Data Receiver

--- BMSDataReceiver.py
+++ BMSDataReceiver.py
@@ -6,16 +6,9 @@ class BMSDataReceiver:
         self.bms_data = []
 
     def read_data_from_sender(self):
-
-        for line in sys.stdin:
-            if len(line) != 1:
-                bms_param = {}
-                print(line.replace('\n', ''))
-                input_data = line.split('\t')
-                bms_param['Temperature'] = input_data[0]
-                bms_param['SoC'] = input_data[1]
-                bms_param['Current'] = input_data[2]
-                self.bms_data.append(bms_param)
+        data = []
+        for _ in range(50):
+            data.append(sys.stdin.readline().strip())
 
 
 receiver = BMSDataReceiver()
